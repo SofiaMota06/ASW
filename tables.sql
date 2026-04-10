b44a97a1dddf6f0a415b568c753d9593eeda289d
SofiaMota06 <sofia.neto.park@gmail.com>
2026-04-10 18:03:32 +0100

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -41,4 +41,42 @@ CREATE TABLE Comments(
     date DATETIME,
 )
 
+
+
+import timestamp
+CREATE TABLE Friends (
+    /*No vosso código Node.js, antes de inserirem na tabela, podem fazer um pequeno truque:
+const [user1, user2] = [idA, idB].sort();*/
+    id_user1 int REFERENCES Users(id_user) ON DELETE cascade,
+    id_user2 int REFERENCES Users(id_user) ON DELETE cascade,
+    PRIMARY KEY (id_user1, id_user2) ,
+    state varchar(16) DEFAULT 'pending' CHECK (state IN ('pending', ' accepted')), /* pending ou accepted -  Para prevenir que entrem outros estados além de pending e accepted */
+    CONSTRAINT users_must_be_different CHECK (id_user1 <> id_user2) /*Para que o mesmo utilizador não se peça em amizade consigo próprio*/
+)
+
+CREATE TABLE Groups (
+    id_group int PRIMARY KEY,
+    name varchar(25) NOT NULL,
+    created_date timestamp DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE Groups_Members (
+    id_group int REFERENCES Groups(id_group) ON DELETE Cascade,
+    id_user int REFERENCES Users(id_user) ON DELETE Cascade,
+    level int DEFAULT 0 CHECK (level IN (0, 1)),
+    PRIMARY KEY (id_group, id_user) 
+    
+)
+
+
+CREATE TABLE Groups_Messages(
+    id_message int PRIMARY KEY,
+    id_group int REFERENCES Groups(id_group) ON DELETE Cascade,
+    id_user int REFERENCES Users(id_user) ON DELETE Cascade,
+    text varchar(9999),
+    file_url varchar(255),  /*O link para o PDF no Cloudinary*/
+    file_type varchar(20), /*pdf ,image*/
+    created_date timestamp DEFAULT CURRENT_TIMESTAMP
+
+)
   
