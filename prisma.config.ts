fa0c838964a6f00e5d33227986ece01f57a6267d
SofiaMota06 <sofia.neto.park@gmail.com>
2026-04-13 17:05:45 +0100

    ja ta amen, N  POR OS NODE MODULES NO GIT

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,10 @@
 
 import 'dotenv/config';
 import { defineConfig, env } from '@prisma/config';
-// SE DER ERRO NO  '@prisma/config'  É NECESSÁRIO INSTALAR O NODE!!! (https://nodejs.org/en/download) e dps no terminal --> npx prisma migrate dev --name init_tabelas
+// SE DER ERRO NO  '@prisma/config'  É NECESSÁRIO INSTALAR O NODE!!! 
+//      (https://nodejs.org/en/download) e dps no terminal --> npx prisma migrate dev --name init_tabelas
+
+// npm install
 export default defineConfig({
   datasource: {
     // Aponta para o url da nossa base de dados PostgreSQL usando o ajudante "env"
