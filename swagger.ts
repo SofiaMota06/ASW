c3a8d080306d6aa562c6f2784378bb2b39569214
Rita <rlab07261@gmail.com>
2026-04-15 14:34:37 +0000

    Add Swagger documentation setup in swagger.tsnao esta completo

---
+++ swagger.ts
@@ -0,0 +1,13 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+const options = {
+definition: {
+openapi: '3.0.0',
+info: {
+title: 'Library API',
+version: '1.0.0',
+description: 'Documentação organizada por ficheiros YAML'
+},
+},
+apis: ['./src/docs/*.yaml'],
+};
+export const specs = swaggerJSDoc(options);
